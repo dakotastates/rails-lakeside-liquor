09566b8b2e21032a5b0c83e58566608157ca25e3
Dakota States <dakota.states@gmail.com>
2021-12-07 20:52:39 -0800

    Admin Routes, controller, views setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  scope '/admin' do
+      devise_for :users, path: '/', skip: [:registrations, :passwords]
+
+      devise_scope :user do
+        authenticated :user do
+          get '/', to: 'admin#index', as: :admin_root
+
+
+        end
+
+        unauthenticated do
+          get '/', to: 'devise/sessions#new'
+
+        end
+      end
+
+    end
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
