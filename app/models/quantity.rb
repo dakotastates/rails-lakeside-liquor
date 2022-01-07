8d8874f9fe03e528d0617b614f29295d44b20ef1
Dakota States <dakota.states@gmail.com>
2022-01-06 16:57:51 -0800

    Find or Create Ingredient, update Footer

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -10,14 +10,14 @@ class Quantity < ApplicationRecord
 
   accepts_nested_attributes_for :ingredient, allow_destroy: true, :reject_if => :all_blank
 
-  # def ingredient_attributes=(ingredient_attributes)
-  #   ingredient_attributes.values.each do |i|
-  #       if i != ""
-  #           ingredient = Ingredient.find_or_create_by(ingredient: i.downcase)
-  #           self.ingredient = ingredient
-  #       end
-  #   end
-  # end
+  def ingredient_attributes=(ingredient_attributes)
+    ingredient_attributes.values.each do |i|
+        if i != ""
+            ingredient = Ingredient.find_or_create_by(ingredient_name: i.downcase)
+            self.ingredient = ingredient
+        end
+    end
+  end
 
   def ingredient_name
     self.ingredient.ingredient if !self.id.nil?
